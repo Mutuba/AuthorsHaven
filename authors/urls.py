ae752f90e6c15f955426418e26929b9f46a940db
Mutuba <danielmutubait@gmail.com>
2021-05-20 21:57:14 +0300

    Fix rabbitMQ url

--- authors/urls.py
+++ authors/urls.py
@@ -23,6 +23,7 @@ import notifications.urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
     path("api/", include("authors.apps.authentication.urls")),
     path("api/articles/", include("authors.apps.articles.urls")),
     path("api/profiles/", include("authors.apps.profiles.urls")),

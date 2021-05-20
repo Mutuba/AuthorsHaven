a42595014376eaf77787f6dfc8f8712ecb4a38e1
Mutuba <danielmutubait@gmail.com>
2021-05-20 18:58:41 +0300

    Add changes

--- authors/urls.py
+++ authors/urls.py
@@ -20,6 +20,7 @@ import notifications.urls
 
 # from mysite.core import views as core_views
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("authors.apps.authentication.urls")),
